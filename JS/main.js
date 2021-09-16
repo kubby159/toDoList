f5556cd52898d22c4250cd1d41d038668991e9f9
kubby159 <kubby159@naver.com>
2021-09-16 23:55:06 +0900

    loginForm Updated

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,21 @@
 
-
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const loginButton = document.querySelector('#login-form button');
+const greeting = document.querySelector('#greeting');
+
 
 
 //login Btn
-function  onLoginBtnClick () {
-    console.log("Hello", loginInput.value);
-   
+function   onLoginSubmit (event) {
+   event.preventDefault(); //브라우저의 기본 동작을 막아준다. 여기서는 submit 시 새로고침되는 걸 막아줌.
+    const username = loginInput.value;
+    loginForm.classList.add('hidden');
+    greeting.innerText = `Hello! ${username}`;
+    greeting.classList.remove('hidden');
 
 }
 
 
-loginButton.addEventListener('click', onLoginBtnClick);
+
+loginForm.addEventListener('submit', onLoginSubmit);
+
