d3dfde130f8aa0bb6ad96f3003fbcc472262aace
kubby159 <kubby159@naver.com>
2021-09-17 11:41:41 +0900

    How to use localStorage

--- JS/main.js
+++ JS/main.js
@@ -6,9 +6,10 @@ const greeting = document.querySelector('#greeting');
 
 
 //login Btn
-function   onLoginSubmit (event) {
+function onLoginSubmit (event) {
    event.preventDefault(); //브라우저의 기본 동작을 막아준다. 여기서는 submit 시 새로고침되는 걸 막아줌.
     const username = loginInput.value;
+    localStorage.setItem("username",username);
     loginForm.classList.add('hidden');
     greeting.innerText = `Hello! ${username}`;
     greeting.classList.remove('hidden');
