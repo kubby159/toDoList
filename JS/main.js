ed7c506c2658d55c564a314c6acc557fa211a70a
kubby159 <kubby159@naver.com>
2021-09-17 12:04:01 +0900

    paintGreetings Function, revised form

--- JS/main.js
+++ JS/main.js
@@ -3,20 +3,41 @@ const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
-
+const USERNAME_KEY = 'username'; //같은 String이 반복될 떄는 오타날 확률이 있음. 그러므로 변수에 담아서 사용하는 것이 안전함.(오류찾기가 쉬움)
 
 //login Btn
 function onLoginSubmit (event) {
    event.preventDefault(); //브라우저의 기본 동작을 막아준다. 여기서는 submit 시 새로고침되는 걸 막아줌.
     const username = loginInput.value;
-    localStorage.setItem("username",username);
+    localStorage.setItem("USERNAME_KEY",username);
     loginForm.classList.add('hidden');
-    greeting.innerText = `Hello! ${username}`;
-    greeting.classList.remove('hidden');
+    paintGreetings(username);
 
+}
+
+//paintGreetings
+
+function paintGreetings(username) {
+
+    greeting.innerText = `Hello! ${username}`; 
+    greeting.classList.remove('hidden');
 }
 
 
 
-loginForm.addEventListener('submit', onLoginSubmit);
+//localstorage 받아오기.
+const savedUserName = localStorage.getItem('USERNAME_KEY');
+
+if(savedUserName === null) {
+
+    //show the form
+
+    loginForm.classList.remove('hidden');
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else  {
+
+    // show the greeting
+    paintGreetings(savedUserName);
+    
+}
 
