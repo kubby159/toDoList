ac94038bb82db7a4060b820fff54933a6c20745e
kubby159 <kubby159@naver.com>
2021-09-17 17:43:52 +0900

    list Remove

--- JS/todo.js
+++ JS/todo.js
@@ -2,12 +2,24 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
 
+function deleteToDo (event) {
+
+    const li = event.target.parentElement;
+    li.remove();
+
+}
+
+
 
 function paintToDo(newTodo) {
 
     const li = document.createElement('li');
     const span = document.createElement('span');
+    const btn = document.createElement('button');
+    btn.innerText = '❌'
+    btn.addEventListener('click', deleteToDo)
     li.appendChild(span);
+    li.appendChild(btn);
     span.innerText = newTodo;
     toDoList.appendChild(li);
 
