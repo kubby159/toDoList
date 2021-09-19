b5cff7fb41a395264d174b904479311a78598d09
kubby159 <kubby159@naver.com>
2021-09-19 23:49:21 +0900

    Reviewed all

--- JS/todo.js
+++ JS/todo.js
@@ -1,53 +1,49 @@
-const toDoForm = document.getElementById('todo-form');
-const toDoInput = toDoForm.querySelector('input');
-const toDoList = document.getElementById('todo-list');
+
+const toDoForm = document.querySelector('#todo-form');
+const toDoInput = toDoForm.querySelector('#todo-form input');
+const toDoList = document.querySelector('#todo-list');
 
 
-let toDos = [];
 
 
-function deleteToDo (event) {
 
-    const li = event.target.parentElement;
-    li.remove();
-
-}
-
-function saveToDos() {
-    localStorage.setItem('toDos',JSON.stringify(toDos));
-}
 
 function paintToDo(newTodo) {
 
-    const li = document.createElement('li');
+    const li = document.createElement('li')
     const span = document.createElement('span');
     const btn = document.createElement('button');
-    btn.innerText = '❌'
-    btn.addEventListener('click', deleteToDo)
+
+   
     li.appendChild(span);
     li.appendChild(btn);
-    span.innerText = newTodo;
-    toDoList.appendChild(li);
-
+    span.innerText = newTodo;    
+    btn.innerText ="❌";
+    btn.addEventListener('click',deleteList)
+   toDoList.appendChild(li);
 }
 
 
-function handleToDoSubmit(event) {
+
+
+function deleteList(event) {
+
+
+    const deleteEvent = event.target.parentNode
+    deleteEvent.remove();
+}
+
+
+
+function handleToDoSubmit (event) {
 
     event.preventDefault();
     const newTodo = toDoInput.value;
-    toDoInput.value="";
-    toDos.push(newTodo);
+    toDoInput.value = "";
     paintToDo(newTodo);
-    saveToDos();
+
 }
 
-toDoForm.addEventListener('submit',handleToDoSubmit)
 
 
-const savedToDos = localStorage.getItem('toDos');
-if(savedToDos !== null) {
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo)
-}
+toDoForm.addEventListener('submit',handleToDoSubmit);
